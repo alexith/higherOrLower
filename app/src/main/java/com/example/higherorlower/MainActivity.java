3caa43c463c6094e5188717e191d96a4418eed25
alexith <akshay.cy2@gmail.com>
2020-05-11 14:03:00 +0530

    update and simple errors

--- app/src/main/java/com/example/higherorlower/MainActivity.java
+++ app/src/main/java/com/example/higherorlower/MainActivity.java
@@ -4,10 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     public void guessInProgress(View view){
+
+    EditText guessedNumber = (EditText) findViewById(R.id.guessedNumber);
+
+        int randomNumber = (int) (Math.random()+1)*2;
+
+
+
 
     }
 
