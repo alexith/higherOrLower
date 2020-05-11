8679e064d7d0a929b01e10326b2018b97f208bdd
alexith <akshay.cy2@gmail.com>
2020-05-11 12:19:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/higherorlower/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.higherorlower;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public class guessInProgress(View view){
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
